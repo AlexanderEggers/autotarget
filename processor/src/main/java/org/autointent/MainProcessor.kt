1b138950206d3435e1b11ead52d3b76dc43425aa
Mordag <alexander.eggers91@gmail.com>
2018-03-29 17:52:07 +1100

    Added missing file

---
+++ processor/src/main/java/org/autointent/MainProcessor.kt
@@ -0,0 +1,58 @@
+package org.autointent
+
+import com.google.auto.service.AutoService
+import org.autointent.annotation.ForActivity
+import org.autointent.annotation.ForActivityProcessor
+import org.autointent.annotation.IntentParameter
+import org.autointent.annotation.IntentParameterProcessor
+import org.autointent.helper.ContextInjectorProcessor
+import org.autointent.helper.ContextProviderProcessor
+import java.io.IOException
+import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
+import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
+
+@AutoService(Processor::class)
+class MainProcessor : AbstractProcessor() {
+
+    var filer: Filer? = null
+    var messager: Messager? = null
+    var elements: Elements? = null
+    var intentParameterMap: HashMap<String, ArrayList<Element>>? = null
+
+    @Synchronized
+    override fun init(processingEnvironment: ProcessingEnvironment) {
+        super.init(processingEnvironment)
+        filer = processingEnvironment.filer
+        messager = processingEnvironment.messager
+        elements = processingEnvironment.elementUtils
+        intentParameterMap = HashMap()
+    }
+
+    override fun process(set: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        try {
+            //Helper processor part - like for the class ContextProvider
+            ContextProviderProcessor().process(filer!!)
+            ContextInjectorProcessor().process(filer!!)
+
+            //Annotation processor part - like for the annotation @ForActivity
+            IntentParameterProcessor().process(this, roundEnv)
+            ForActivityProcessor().process(this, roundEnv)
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return true
+    }
+
+    override fun getSupportedAnnotationTypes(): MutableSet<String> {
+        return mutableSetOf(ForActivity::class.java.name, IntentParameter::class.java.name)
+    }
+
+    override fun getSupportedSourceVersion(): SourceVersion {
+        return SourceVersion.latest()
+    }
+}
