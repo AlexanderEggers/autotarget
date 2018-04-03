f1b01b76888904b669fe7a9ca425cc4bd421312a
Mordag <alexander.eggers91@gmail.com>
2018-04-03 13:49:28 +1000

    Added first test for the annotation ForAcitivty

--- examples/src/main/java/org/autointent/MainActivity.kt
+++ examples/src/main/java/org/autointent/MainActivity.kt
@@ -1,20 +1,21 @@
 package org.autointent
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
-import org.autointent.annotation.ForActivity
-import org.autointent.annotation.IntentParameter
+import android.widget.Button
 import org.autointent.generated.ActivityService
 import org.autointent.generated.ContextInjector
 
-@ForActivity
-@IntentParameter(valueKey = "test", valueType = String::class, valueName = "myTestValue")
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         ContextInjector.inject(this)
-        ActivityService().showMainActivity("test")
+
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            ActivityService().showNextActivity("Test successful!")
+        }
     }
 }
---
+++ examples/src/main/java/org/autointent/NextActivity.kt
@@ -0,0 +1,19 @@
+package org.autointent
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import org.autointent.annotation.ForActivity
+import org.autointent.annotation.IntentParameter
+
+@ForActivity
+@IntentParameter(valueKey = "TEXT_KEY", valueType = String::class, valueName = "text")
+class NextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = intent.extras.getString("TEXT_KEY")
+    }
+}
